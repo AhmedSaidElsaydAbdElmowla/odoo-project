40b05f23d15a3bf41ccebcc3d742454324ee115f
Ahmed Saeed El-Sayed <92992513+AhmedSaidElsaydAbdElmowla@users.noreply.github.com>
2024-11-23 19:32:04 +0200

    Add files via upload

---
+++ customizatoin/sera/__manifast__.py
@@ -0,0 +1,21 @@
+{
+
+    'name': 'sera',
+    'version': '0.2.0',
+    'summary': 'sera',
+    'sequence': 5,
+    'description': """
+    
+    """,
+    'author': 'Odooistic',
+    'company': 'Company Name',
+    'website': 'https://www.website.com',
+    'category': 'customizatoin',
+    'depends': ['base'],
+    'data': [
+        # 'security/ir.model.access.csv',
+    
+
+   ],
+    'application': True,
+}
